34829f471920e742afa61c5bd012c70b582f7814
Alessia Lacitignola <80158378+alessialaci@users.noreply.github.com>
2022-11-21 16:36:03 +0100

    Add files via upload

---
+++ Esercizio1/Giocatori.js
@@ -0,0 +1,22 @@
+"use strict";
+var numeroCasuale = Math.round(Math.random() * (100 - 1) + 1);
+var giocatore1 = Math.round(Math.random() * (100 - 1) + 1);
+var giocatore2 = Math.round(Math.random() * (100 - 1) + 1);
+console.log('Il numero estratto è ' + numeroCasuale);
+console.log('Il numero del giocatore 1 è ' + giocatore1);
+console.log('Il numero del giocatore 2 è ' + giocatore2);
+if (giocatore1 == numeroCasuale) {
+    console.log('Ha vinto il giocatore 1!');
+}
+else if (giocatore2 == numeroCasuale) {
+    console.log('Ha vinto il giocatore 2!');
+}
+else if (giocatore1 == giocatore2) {
+    console.log('Entrambi avete dato lo stesso numero, riprovate!');
+}
+else if ((numeroCasuale - giocatore1) > (numeroCasuale - giocatore2)) {
+    console.log('Nessuno dei due giocatori ha indovinato il numero, ma il giocatore 1 si è avvicinato di più!');
+}
+else if ((numeroCasuale - giocatore1) < (numeroCasuale - giocatore2)) {
+    console.log('Nessuno dei due giocatori ha indovinato il numero, ma il giocatore 2 si è avvicinato di più!');
+}
